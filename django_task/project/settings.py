6680ad8be649e54b1d3d31f40be01681db99b513
sathwikgaje <85938598+sathwikgaje@users.noreply.github.com>
2022-03-25 21:48:42 +0530

    Update settings.py

--- django_task/project/settings.py
+++ django_task/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@sa2q+mw=q39&&$sqmn1q4nw5obe(kgop)lnol%2j9o$^l299x'
+SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database',
-        'HOST' : 'localhost',
-        'USER': 'root',
-        'PORT' : '3306',
-        'PASSWORD':'sahruday@1'
+        'NAME': '',
+        'HOST' : '',
+        'USER': '',
+        'PORT' : '',
+        'PASSWORD':''
     }
 }
 
